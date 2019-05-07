12bf0f1ea46bea61ec35d2bb95e14ad1fb3517c6
Trey Dockendorf <tdockendorf@osc.edu>
2019-05-07 13:38:52 -0400

    Rubocop auto_correct

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -17,4 +17,4 @@ end
 
 # 'spec_overrides' from sync.yml will appear below this line
 proj_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
-copy_root_module_to(hosts, :source => File.join(proj_root, 'tests/site_munge'), :module_name => 'site_munge', :ignore_list => [])
+copy_root_module_to(hosts, source: File.join(proj_root, 'tests/site_munge'), module_name: 'site_munge', ignore_list: [])
