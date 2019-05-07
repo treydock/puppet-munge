4cc0d2976d185c84a86056f8a144cd65d506e9ea
Trey Dockendorf <tdockendorf@osc.edu>
2019-05-07 11:04:11 -0400

    pdk convert

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -3,13 +3,18 @@ require 'beaker-puppet'
 require 'beaker/puppet_install_helper'
 require 'beaker/module_install_helper'
 
+dir = File.expand_path(File.dirname(__FILE__))
+Dir["#{dir}/acceptance/shared_examples/**/*.rb"].sort.each { |f| require f }
+
 run_puppet_install_helper
 install_module_on(hosts)
 install_module_dependencies_on(hosts)
-proj_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
-copy_root_module_to(hosts, :source => File.join(proj_root, 'tests/site_munge'), :module_name => 'site_munge', :ignore_list => [])
 
 RSpec.configure do |c|
   # Readable test descriptions
   c.formatter = :documentation
 end
+
+# 'spec_overrides' from sync.yml will appear below this line
+proj_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+copy_root_module_to(hosts, :source => File.join(proj_root, 'tests/site_munge'), :module_name => 'site_munge', :ignore_list => [])
